3ecbd76575ad3215b97801f143c9d2ac9b24df63
Magnum Adelano <magnemcs@gmail.com>
2017-11-23 22:10:48 -0300

    teste rest

--- src/test/java/com/portifolio/magnum/SpringBootWsApplicationTests.java
+++ src/test/java/com/portifolio/magnum/SpringBootWsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringToosWsApplicationTests {
+public class SpringBootWsApplicationTests {
 
 	@Test
 	public void contextLoads() {
