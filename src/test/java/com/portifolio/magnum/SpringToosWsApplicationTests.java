2f56ba866591659f009cd97cfb4becd58ddfac7d
Magnum Adelano <magnemcs@gmail.com>
2017-11-23 21:39:55 -0300

    spring-boot-ws

---
+++ src/test/java/com/portifolio/magnum/SpringToosWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.portifolio.magnum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringToosWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
