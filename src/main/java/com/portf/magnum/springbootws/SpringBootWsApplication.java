6f710f829cb83e6896cf9de88306f23def4eccf6
Magnum Adelano <magnemcs@gmail.com>
2017-11-23 22:10:24 -0300

    teste rest

---
+++ src/main/java/com/portf/magnum/springbootws/SpringBootWsApplication.java
@@ -0,0 +1,12 @@
+package com.portf.magnum.springbootws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWsApplication.class, args);
+	}
+}
