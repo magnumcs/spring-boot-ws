1cb557cf3aadef7726670bf9bb6e3c426f7caaa0
Magnum Adelano <magnemcs@gmail.com>
2017-11-23 22:39:25 -0300

    metodo listar categorias

--- src/main/java/com/portf/magnum/springbootws/resources/CategoriaResource.java
+++ src/main/java/com/portf/magnum/springbootws/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.portf.magnum.springbootws.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.portf.magnum.springbootws.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,18 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funcionou";
+	public List<Categoria> listar() {
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		categorias.add(c1);
+		categorias.add(c2);
+		
+		return categorias;
+		
 	}
 
 }
