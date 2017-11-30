fb1a67b6acec441b892954fa4ee18292cbf15be2
Magnum Adelano <magnemcs@gmail.com>
2017-11-29 23:56:57 -0300

    Implementação dominio produto e

--- src/main/java/com/portf/magnum/springbootws/SpringBootWsApplication.java
+++ src/main/java/com/portf/magnum/springbootws/SpringBootWsApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.portf.magnum.springbootws.domain.Categoria;
+import com.portf.magnum.springbootws.domain.Produto;
 import com.portf.magnum.springbootws.repository.CategoriaRepository;
+import com.portf.magnum.springbootws.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringBootWsApplication implements CommandLineRunner {
@@ -16,16 +18,32 @@ public class SpringBootWsApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		Categoria c1 = new Categoria(1, "Informática");
-		Categoria c2 = new Categoria(2, "Escritório");
+		
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressoa", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
 		
 		categoriaRepository.save(Arrays.asList(c1, c2));
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 	}
 	
 }
--- src/main/java/com/portf/magnum/springbootws/domain/Categoria.java
+++ src/main/java/com/portf/magnum/springbootws/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.portf.magnum.springbootws.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -73,4 +79,13 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 }
